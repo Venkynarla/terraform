dbbf47a7de32817debaacae3ddc763c1e75c3127
root <root@ip-172-31-44-65.ec2.internal>
2020-04-13 19:13:15 +0000

    text added

--- instance.tf
+++ instance.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "web" {
   ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.instancesg.name}"] 
+  user_data = <<-EOF
+             #! /bin/bash
+                sudo yum install httpd -y
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                echo "Welcome to first terraform website for testing" > /var/www/html/index.html
+                EOF         
+
+
 tags          = {
     Name        = "web"
     Location  = "Delhi"
