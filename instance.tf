01ac0c1412d0dcaa3445d57b7d079f37906cb413
root <root@ip-172-31-44-65.ec2.internal>
2020-04-13 18:39:47 +0000

    file added

---
+++ instance.tf
@@ -0,0 +1,74 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instancesg.name}"] 
+tags          = {
+    Name        = "web"
+    Location  = "Delhi"
+  }
+}
+
+#instance security group for..
+
+resource "aws_security_group" "instancesg" {
+  name        = "instancesg"
+  description = "Created the security group for ec2 instance"
+  vpc_id      = "vpc-ced84ab4"
+
+  ingress {
+   
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+
+
+  tags = {
+    Name = "instancesg"
+  }
+}
+
+
+
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+access_key = "AKIAQI73CH2Q7WZK4TCG"
+secret_key = "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"
+region = "us-east-1"
+}
